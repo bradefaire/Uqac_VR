7eac804224995ef4ce0dc1fb2d7e9e80a194d8e4
Niels <nkristen@free.fr>
2025-03-24 18:58:43 -0400

    Normalized distance computation v0.1

--- Quickdraw/Assets/Scripts/Revolver.cs
+++ Quickdraw/Assets/Scripts/Revolver.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Revolver : MonoBehaviour
@@ -5,30 +7,35 @@ public class Revolver : MonoBehaviour
     [SerializeField] private Transform muzzle;
     [SerializeField] private LayerMask mask;
     private CsvSaver csvSaver;
-
     private LineRenderer line;
+    private int shots;
+    private List<float> impactDistances;
     
     private void Start()
     {
         line = GetComponent<LineRenderer>();
         csvSaver = GetComponent<CsvSaver>();
+        impactDistances = new List<float>();
     }
 
     public void Select()
     {
+        shots = 0;
         Debug.Log("Select");
         // Disable controller visual and lock grip
     }
     
     public void Deselect()
     {
-        Debug.Log("Deselect");
+        Debug.Log("Deselect");      
+        csvSaver.SaveTargetShotToCsv(
+            impactDistances.Count > 0 ? impactDistances.Average() : 0,
+            impactDistances.Count > 0 ? ComputeStandardDeviation(impactDistances) : 0
+        );
     }
 
     public void Fire()
     {
-        Debug.Log("Bang");
-        
         line.SetPosition(0, muzzle.position);
         line.SetPosition(1, muzzle.position + muzzle.forward * 100f);
         if (Physics.Raycast(muzzle.position, muzzle.forward, out RaycastHit hit, 100f, mask))
@@ -36,17 +43,27 @@ public class Revolver : MonoBehaviour
             line.startColor = line.endColor = Color.green;
             Vector3 impactPosition = hit.point;
             Vector3 targetPosition = hit.transform.position;
+            float targetRadius = hit.collider.bounds.extents.magnitude;
             float distance = Vector3.Distance(impactPosition, targetPosition);
-            Debug.Log("Hit ! Distance: " + distance);
-            csvSaver.SaveTargetShotToCsv(
-                distance,
-                Vector3.SignedAngle(impactPosition, targetPosition, Vector3.right)
-            );
+            float normalizedDistance = distance / targetRadius;
+            Debug.Log("Target radius : " + targetRadius);
+            impactDistances.Add(normalizedDistance);
+            //Debug.Log("Hit ! Distance : " + normalizedDistance);
+            Debug.Log("Raw distance : " + distance);
+            shots++; 
         }
         else
         {
             line.startColor = line.endColor = Color.red;
-            Debug.Log("Miss !");
         }
     }
+
+    private float ComputeStandardDeviation(List<float> values)
+        {
+            if (values.Count == 0) return 0;
+    
+            float average = values.Average();
+            float sumOfSquaresOfDifferences = values.Select(val => (val - average) * (val - average)).Sum();
+            return Mathf.Sqrt(sumOfSquaresOfDifferences / values.Count);
+        }
 }
